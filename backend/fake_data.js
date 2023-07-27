f622f088cb001e103d7f2d7f911f7ba2ecd08d12
Alexander Muli <mulingealexmuli@gmail.com>
2023-07-27 18:13:59 +0300

    puppeteer prac

---
+++ backend/fake_data.js
@@ -0,0 +1,112 @@
+module.exports = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@example.com",
+    age: 28,
+    financialDetails: {
+      accountNumber: "1234567890",
+      bankName: "Example Bank",
+      accountType: "savings",
+    },
+  },
+  {
+    firstName: "Alice",
+    lastName: "Smith",
+    email: "alice.smith@example.com",
+    age: 34,
+    financialDetails: {
+      accountNumber: "9876543210",
+      bankName: "Sample Bank",
+      accountType: "current",
+    },
+  },
+  {
+    firstName: "Michael",
+    lastName: "Johnson",
+    email: "michael.johnson@example.com",
+    age: 25,
+    financialDetails: {
+      accountNumber: "4444333322",
+      bankName: "Mock Bank",
+      accountType: "fixed",
+    },
+  },
+  {
+    firstName: "Emily",
+    lastName: "Brown",
+    email: "emily.brown@example.com",
+    age: 40,
+    financialDetails: {
+      accountNumber: "1111222233",
+      bankName: "Test Bank",
+      accountType: "savings",
+    },
+  },
+  {
+    firstName: "James",
+    lastName: "Williams",
+    email: "james.williams@example.com",
+    age: 22,
+    financialDetails: {
+      accountNumber: "9999888877",
+      bankName: "Trial Bank",
+      accountType: "current",
+    },
+  },
+  {
+    firstName: "Emma",
+    lastName: "Jones",
+    email: "emma.jones@example.com",
+    age: 29,
+    financialDetails: {
+      accountNumber: "5555666677",
+      bankName: "Demo Bank",
+      accountType: "fixed",
+    },
+  },
+  {
+    firstName: "William",
+    lastName: "Brown",
+    email: "william.brown@example.com",
+    age: 32,
+    financialDetails: {
+      accountNumber: "9999444455",
+      bankName: "Test Bank",
+      accountType: "savings",
+    },
+  },
+  {
+    firstName: "Olivia",
+    lastName: "Miller",
+    email: "olivia.miller@example.com",
+    age: 37,
+    financialDetails: {
+      accountNumber: "7777888899",
+      bankName: "Demo Bank",
+      accountType: "current",
+    },
+  },
+  {
+    firstName: "Liam",
+    lastName: "Wilson",
+    email: "liam.wilson@example.com",
+    age: 31,
+    financialDetails: {
+      accountNumber: "1234987654",
+      bankName: "Example Bank",
+      accountType: "fixed",
+    },
+  },
+  {
+    firstName: "Ava",
+    lastName: "Taylor",
+    email: "ava.taylor@example.com",
+    age: 27,
+    financialDetails: {
+      accountNumber: "9080706050",
+      bankName: "Sample Bank",
+      accountType: "savings",
+    },
+  },
+];
